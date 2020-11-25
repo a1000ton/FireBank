7e466531c5254b28f50bd78de4b32c7b61ce7f85
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-25 15:53:44 -0300

    Enable migrations

---
+++ FireBank.Infra.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace FireBank.Infra.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FireBank.Infra.Data.Configuration.FireBankContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FireBank.Infra.Data.Configuration.FireBankContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

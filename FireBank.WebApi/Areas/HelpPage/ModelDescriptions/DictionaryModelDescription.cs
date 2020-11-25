375c7ef8da73322cd5115fe01bed8259639c15bc
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-25 13:43:49 -0300

    Create WebApi project to use Depedency Injection

---
+++ FireBank.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FireBank.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FireBank.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FireBank.WebApi.WebApiApplication" Language="C#" %>

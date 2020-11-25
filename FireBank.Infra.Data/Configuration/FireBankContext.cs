955a056fea1caa3fbf5e84f33d4d59645fa06c24
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-25 12:12:48 -0300

    Create the context and the dbSets

---
+++ FireBank.Infra.Data/Configuration/FireBankContext.cs
@@ -0,0 +1,28 @@
+﻿using FireBank.Domain.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FireBank.Infra.Data.Configuration
+{
+    public class FireBankContext : DbContext
+    {
+        public FireBankContext() : base("Data Source=(localdb)\\ProjectsV13;Initial Catalog=FireBank;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+        }
+    }
+}

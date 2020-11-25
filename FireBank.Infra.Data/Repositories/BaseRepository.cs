c52f959bc1e36103067d1915fdabae70e6a2669e
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-25 14:53:26 -0300

    Created BaseRepository

---
+++ FireBank.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using FireBank.Domain.Interfaces.Repository;
+using FireBank.Infra.Data.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireBank.Infra.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        private readonly FireBankContext _db;
+
+        public BaseRepository(FireBankContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

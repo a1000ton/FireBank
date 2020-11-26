9fcf3bbac68b31237c05db700e67a3926b60853f
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-26 18:42:40 -0300

    Create interfaces and classes to account creation

---
+++ FireBank.Application/Applications/AccountApplication.cs
@@ -0,0 +1,31 @@
+﻿using FireBank.Application.Applications.Interfaces;
+using FireBank.Application.Models;
+using FireBank.Domain.Interfaces.Service;
+using System;
+
+namespace FireBank.Application.Applications
+{
+    public class AccountApplication : IAccountApplication
+    {
+        private readonly IAccountService _service;
+        public AccountApplication(IAccountService service)
+        {
+            _service = service;
+        }
+
+        public AccountCreatedModel CreateBusinessAccount(BusinessAccountCreationModel account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountCreatedModel CreateGiroAccount(GiroAccountCreationModel account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountCreatedModel CreateStudentAccount(StudentAccountCreationModel account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FireBank.Application/Applications/Interfaces/IAccountApplication.cs
@@ -0,0 +1,11 @@
+﻿using FireBank.Application.Models;
+
+namespace FireBank.Application.Applications.Interfaces
+{
+    public interface IAccountApplication
+    {
+        AccountCreatedModel CreateBusinessAccount(BusinessAccountCreationModel account);
+        AccountCreatedModel CreateStudentAccount(StudentAccountCreationModel account);
+        AccountCreatedModel CreateGiroAccount(GiroAccountCreationModel account);
+    }
+}

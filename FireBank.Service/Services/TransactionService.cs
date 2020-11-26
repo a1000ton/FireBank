b59c1739d10e044c8e11d5494cb5f9ef28f82b4b
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-26 03:53:07 -0300

    Create logic to withdrawal and deposit, create logic to Add new transactions

--- FireBank.Service/Services/TransactionService.cs
+++ FireBank.Service/Services/TransactionService.cs
@@ -8,15 +8,28 @@ namespace FireBank.Service.Services
     public class TransactionService : ITransactionService
     {
         private readonly ITransactionRepository _repository;
+        private readonly IBaseAccountService<BaseAccount> _service;
 
-        public TransactionService(ITransactionRepository repository)
+        public TransactionService(ITransactionRepository repository, IBaseAccountService<BaseAccount> service)
         {
             _repository = repository;
+            _service = service;
         }
 
         public Transaction Add(Transaction obj)
         {
-            return _repository.Add(obj);
+            var accountId = obj.Account.Id;
+            var currentBalance = _service.GetBalance(accountId);
+
+            if (obj.Type == TransactionType.Deposit)
+                currentBalance = Deposit(obj.Amount, currentBalance);
+            else if (obj.Type == TransactionType.Withdrawal)
+                currentBalance = Withdrawal(obj.Amount, currentBalance);
+
+            if (CanCompleteTransaction(currentBalance, accountId))
+                return _repository.Add(obj);
+
+            return null;
         }
 
         public IEnumerable<Transaction> GetAll()
@@ -27,6 +40,21 @@ namespace FireBank.Service.Services
         public Transaction GetById(int id)
         {
             return _repository.GetById(id);
+        }
+
+        private int Withdrawal(int amount, int balance)
+        {
+            return balance -= amount;
+        }
+
+        private int Deposit(int amount, int balance)
+        {
+            return balance += amount;
+        }
+
+        private bool CanCompleteTransaction(int balance, int accountId)
+        {
+            return _service.BalanceIsValid(balance, accountId);
         }
     }
 }

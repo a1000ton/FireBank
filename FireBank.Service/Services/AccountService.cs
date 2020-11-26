49e2ef06df27691490d46b4c314af4597549fc60
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-25 21:32:36 -0300

    Create Account and Transaction Services

---
+++ FireBank.Service/Services/AccountService.cs
@@ -0,0 +1,22 @@
+﻿using FireBank.Domain.Entities;
+using FireBank.Domain.Interfaces.Repository;
+using FireBank.Domain.Interfaces.Service;
+using System;
+
+namespace FireBank.Service.Services
+{
+    public class AccountService : BaseService<Account>, IAccountService
+    {
+        private readonly IAccountRepository _repository;
+
+        public AccountService(IAccountRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+
+        public int GetBalance(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FireBank.Service/Services/TransactionService.cs
@@ -0,0 +1,14 @@
+﻿using FireBank.Domain.Entities;
+using FireBank.Domain.Interfaces.Repository;
+using FireBank.Domain.Interfaces.Service;
+
+namespace FireBank.Service.Services
+{
+    public class TransactionService : BaseService<Transaction>, ITransactionService
+    {
+        public TransactionService(ITransactionRepository repository) : base(repository)
+        {
+
+        }
+    }
+}

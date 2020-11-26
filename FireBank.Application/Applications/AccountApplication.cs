b141e54ffb778d78ce63f3b23965b7bf71cf97f9
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-26 19:09:05 -0300

    Create Implementation and tests to new Business Account on Application

--- FireBank.Application/Applications/AccountApplication.cs
+++ FireBank.Application/Applications/AccountApplication.cs
@@ -1,5 +1,6 @@
 ﻿using FireBank.Application.Applications.Interfaces;
 using FireBank.Application.Models;
+using FireBank.Domain.Entities;
 using FireBank.Domain.Interfaces.Service;
 using System;
 
@@ -15,7 +16,26 @@ namespace FireBank.Application.Applications
 
         public AccountCreatedModel CreateBusinessAccount(BusinessAccountCreationModel account)
         {
-            throw new NotImplementedException();
+            var accountEntity = new Account()
+            {
+                Name = account.Name,
+                AccountType = new BusinessAccount()
+                {
+                    BusinessId = account.BusinessId
+                }
+            };
+
+            var addedAccount = _service.Add(accountEntity);
+            var businessAccount = (BusinessAccount)addedAccount.AccountType;
+
+            return new AccountCreatedModel()
+            {
+                Name = addedAccount.Name,
+                Type = new BusinessAccountCreatedModel()
+                {
+                    BusinessId = businessAccount.BusinessId
+                }
+            };
         }
 
         public AccountCreatedModel CreateGiroAccount(GiroAccountCreationModel account)
---
+++ FireBank.Tests/App/AccountApplicationTests.cs
@@ -0,0 +1,49 @@
+﻿using Effort;
+using FireBank.Application.Applications;
+using FireBank.Application.Models;
+using FireBank.Domain.Entities;
+using FireBank.Infra.Data.Configuration;
+using FireBank.Infra.Data.Repositories;
+using FireBank.Service.Services;
+using System;
+using Xunit;
+
+namespace FireBank.Tests.App
+{
+    public class AccountApplicationTests
+    {
+        [Fact]
+        public void Create_WhenPassValidBusinessAccount_ShouldCreateTheNewAccount()
+        {
+            var connection = DbConnectionFactory.CreateTransient();
+
+            using (var context = new FireBankContext(connection))
+            {
+                var businessId = 10;
+                var name = Guid.NewGuid().ToString();
+
+                var accountToBeCreated = new BusinessAccountCreationModel()
+                {
+                    BusinessId = businessId,
+                    Name = name,
+                };
+
+                var expectedBusinessAccount = new BusinessAccountCreatedModel()
+                {
+                    BusinessId = businessId,
+                };
+
+                var accountRepository = new AccountRepository(context);
+                var accountService = new AccountService(accountRepository);
+                var accountApp = new AccountApplication(accountService);
+
+                var addedAccount = accountApp.CreateBusinessAccount(accountToBeCreated);
+                var addedAccountType = (BusinessAccountCreatedModel)addedAccount.Type;
+
+                Assert.Equal(addedAccount.Name, name);
+                Assert.Equal(addedAccount.Type.GetType(), expectedBusinessAccount.GetType());
+                Assert.Equal(addedAccountType.BusinessId, expectedBusinessAccount.BusinessId);
+            }
+        }
+    }
+}

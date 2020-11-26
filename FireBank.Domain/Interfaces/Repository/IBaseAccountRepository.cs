1d62731950aee5445dbcb523171656dbf14e7d98
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-26 03:50:22 -0300

    Add baseAccount

--- FireBank.Domain/Interfaces/Repository/IBaseAccountRepository.cs
+++ FireBank.Domain/Interfaces/Repository/IBaseAccountRepository.cs
@@ -5,7 +5,6 @@ namespace FireBank.Domain.Interfaces.Repository
     public interface IBaseAccountRepository<TEntity> where TEntity : class
     {
         int GetBalance(int accountId);
-
         TEntity Add(TEntity obj);
         TEntity GetById(int id);
         IEnumerable<TEntity> GetAll();

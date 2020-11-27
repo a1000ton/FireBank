9d02bdc12a0919e9f6138b4c7d6fff13ecea63da
Amilton Baracy <amilton@bempromotora.com.br>
2020-11-26 21:24:38 -0300

    Add controllers to creation of Student and Giro Accounts

---
+++ FireBank.WebApi/Controllers/GiroAccountController.cs
@@ -0,0 +1,21 @@
+﻿using FireBank.Application.Applications.Interfaces;
+using FireBank.Application.Models;
+using System.Web.Http;
+
+namespace FireBank.WebApi.Controllers
+{
+    public class GiroAccountController : ApiController
+    {
+        private readonly IAccountApplication _app;
+        public GiroAccountController(IAccountApplication app)
+        {
+            _app = app;
+        }
+
+        [HttpPost]
+        public AccountCreatedModel Post([FromBody] GiroAccountCreationModel account)
+        {
+            return _app.CreateGiroAccount(account);
+        }
+    }
+}
---
+++ FireBank.WebApi/Controllers/StudentAccountController.cs
@@ -0,0 +1,21 @@
+﻿using FireBank.Application.Applications.Interfaces;
+using FireBank.Application.Models;
+using System.Web.Http;
+
+namespace FireBank.WebApi.Controllers
+{
+    public class StudentAccountController : ApiController
+    {
+        private readonly IAccountApplication _app;
+        public StudentAccountController(IAccountApplication app)
+        {
+            _app = app;
+        }
+
+        [HttpPost]
+        public AccountCreatedModel Post([FromBody] StudentAccountCreationModel account)
+        {
+            return _app.CreateStudentAccount(account);
+        }
+    }
+}
